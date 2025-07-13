6f1d84797fead37b92e9a95de728812c4f80edf8
YoussefAbdalnaby <86475811+YoussefAbdalnaby@users.noreply.github.com>
2025-07-13 07:51:29 +0300

    Update __manifest__.py

--- todo_module1/__manifest__.py
+++ todo_module1/__manifest__.py
@@ -13,7 +13,7 @@ Features:
 * Simple views for easy task tracking
 * Basic workflow for task completion
     """,
-    'author': 'Your Name',
+    'author': 'Youssef Abdalnaby',
     'depends': ['base'],
     'data': [
         'security/security.xml',
